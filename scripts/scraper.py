aef092a5152bdda2a6896b6148062cc5b2dc0e75
wendtalexander <wendtalexander@protonmail.com>
2022-06-01 21:58:00 +0200

    recipe in json

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,5 +1,17 @@
 from recipe_scrapers import scrape_me
+import json
+
+
 scraper = scrape_me('https://www.chefkoch.de/rezepte/100961040660758/Curry-Gemuese-mit-Tofu.html')
+ingr_data = scraper.ingredients()
+recipe_name = scraper.title()
+ingr_data = [i.split(',', 1)[0].strip() for i in ingr_data]
+
+recipe_dict = {'name':[], 'ingredients':[]}
+recipe_dict['name'].append(recipe_name)
+recipe_dict['ingredients'].append(ingr_data)
 
 
-print(scraper.ingredients())
+with open('ingr_data.json', 'w', encoding='utf-8') as f:
+        json.dump(recipe_dict, f, ensure_ascii=False, indent=4)
+
