7bc6d500d5bd5a6d463085877290f09fef82585f
wendtalexander <wendtalexander@protonmail.com>
2022-06-03 12:59:19 +0200

    Refactoring

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,17 +1,41 @@
 from recipe_scrapers import scrape_me
 import json
+from IPython import embed
+
+def get_recipe(link):
+        scraper = scrape_me(link)
+        ingr_data = scraper.ingredients()
+        recipe_name = scraper.title()
+        
+        return ingr_data, recipe_name
 
 
-scraper = scrape_me('https://www.chefkoch.de/rezepte/100961040660758/Curry-Gemuese-mit-Tofu.html')
-ingr_data = scraper.ingredients()
-recipe_name = scraper.title()
-ingr_data = [i.split(',', 1)[0].strip() for i in ingr_data]
+def formating_recipe(link):
+        ingr_data, recipe_name = get_recipe(link) 
+        ingr_data = [i.split(',', 1)[0].strip() for i in ingr_data]
+        recipe_dict = {'name':[], 'ingredients':[]}
+        recipe_dict['name'].append(recipe_name)
+        recipe_dict['ingredients'].extend(ingr_data)
 
-recipe_dict = {'name':[], 'ingredients':[]}
-recipe_dict['name'].append(recipe_name)
-recipe_dict['ingredients'].append(ingr_data)
+        return recipe_dict
 
 
-with open('ingr_data.json', 'w', encoding='utf-8') as f:
-        json.dump(recipe_dict, f, ensure_ascii=False, indent=4)
+def recipe_to_json(link):
+        recipe_dict = formating_recipe(link)
+        filename = 'recipe.json'
+        with open(filename, 'w', encoding='utf-8') as f:
+                json.dump(recipe_dict, f, ensure_ascii=False, indent=4)
 
+
+def testing_if_recipe_in_json(new_link):
+        filename = 'recipe.json'
+        _, recipe_name_new = get_recipe(new_link)
+        with open(filename) as f:
+                recipe= json.load(f)
+                embed()
+                exit()
+
+recipe_to_json('https://www.chefkoch.de/rezepte/1159101222073150/Gerolltes-Leberkaese-Cordon-bleu.html')
+testing_if_recipe_in_json('https://www.chefkoch.de/rezepte/1159101222073150/Gerolltes-Leberkaese-Cordon-bleu.html')
+
+        
