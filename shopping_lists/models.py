2a607494691cc8e0561914d58625a6ff5032b3c8
wendtalexander <wendtalexander@protonmail.com>
2022-09-26 22:49:10 +0200

    making an App, Database in Django

--- shopping_lists/models.py
+++ shopping_lists/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ShoppingLink(models.Model):
+    """ Input of the Recipe link """
+    recipe_link = models.URLField(max_length=200)
+    
+    def __str__(self):
+        """Return a link representation of the model.""" 
+        return self.recipe_link
