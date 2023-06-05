2399edf2b81fe6a8908b8b72041de83e1846a14e
wendtalexander <wendtalexander@protonmail.com>
2023-06-05 21:52:31 +0200

    implementing dataframes for ingredients, recips
    Co-authored-by: Jan <JanMStraub@users.noreply.github.com>

---
+++ scripts/main.py
@@ -0,0 +1,8 @@
+# Imports
+from scraper import create_sql
+
+if __name__ == '__main__':
+    print("Program started")
+    create_sql()
+    print("Progam ended")
+    
--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,58 +1,87 @@
 from recipe_scrapers import scrape_me
-import json
+import pandas as pd
 from IPython import embed
 
+"""
+TODOs
+    * schreiben Update Funktion für DF
+    * migrieren DF in SQL
+    * Unittests
+    * Kommentare für Funktionen
+    * Beschreibung
+"""
+
 def get_recipe(link):
+    """
+    Retrieves the ingredients and recipe name from a given recipe link.
+
+    Args:
+        link (str): The URL of the recipe page to scrape.
+
+    Returns:
+        tuple: A tuple containing two elements - the list of ingredients and the recipe name.
+
+    Raises:
+        Any exceptions raised during the scraping process.
+
+    """
+
+    # Initialize a web scraper for the provided link
     scraper = scrape_me(link)
-    ingr_data = scraper.ingredients()
+
+    # Extract the list of ingredients from the scraper
+    ingredients = scraper.ingredients()
+
+    # Extract the recipe name from the scraper
     recipe_name = scraper.title()
-    
-    return ingr_data, recipe_name
+
+    # Return the ingredients and recipe name as a tuple
+    return ingredients, recipe_name
+
+
 
 
 def formating_recipe(link):
-    ingr_data, recipe_name = get_recipe(link) 
-    ingr_data = [i.split(',', 1)[0].strip() for i in ingr_data]
-    recipe_dict = {'name':[], 'ingredients':[]}
-    recipe_dict['name'].append(recipe_name)
-    recipe_dict['ingredients'].extend(ingr_data)
+    ingredients, recipe_name = get_recipe(link) 
+    ingredients = [item.split(',', 1)[0].strip() for item in ingredients]
     
-    return recipe_dict
+    menge = []
+    einheit = []
+    ingredient = []
 
+    for item in ingredients:
+        # split the string into a list of words
+        words = item.split()
+        if len(words) == 3: 
+            menge.append(words[0])
+            einheit.append(words[1])
+            ingredient.append(words[2])  
 
-def creating_json(link):
-    recipe_js = {}
-    recipe_dict = formating_recipe(link)
-    recipe_js[f'{link}'] = {}
-    recipe_js[f'{link}'].update(recipe_dict)
-    filename = 'recipe.json'
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(recipe_js, f, ensure_ascii=False, indent=4)
+        elif len(words) == 2: 
+            menge.append(words[0])
+            einheit.append('')
+            ingredient.append(words[1])
 
-
-def update_json(link):
-    recipe_dict = formating_recipe(link)
-    filename = 'recipe.json'
-    with open(filename) as f:
-        recipe = json.load(f)
-    for i in recipe.keys():
-        if i == link:
-            print('recipe already in database')
+        elif len(words) == 1:
+            menge.append('')
+            einheit.append('')
+            ingredient.append(words[0])
         else:
-            recipe[f'{link}'] = {}
-            recipe[f'{link}'].update(recipe_dict)
+            raise ValueError('There are no ingredients!')
+    # create an index for the recipe for the identification in th
+    # e database
+    index = 0
+    
+    df_index = pd.DataFrame({"Name": recipe_name, "ID": [index]})
 
-            #with open(filename, 'w', encoding='utf-8') as f:
-            #    json.dump(recipe, f, ensure_ascii=False, indent=4)
-            
-            print(f'new recipe added')
+    df = pd.DataFrame({"Zutat": ingredient, 
+                    "Menge": menge, 
+                    "Einheit": einheit, 
+                    "ID": len(ingredient)*[index]})
 
-
-#creating_json('https://www.chefkoch.de/rezepte/1255131230975627/Traenenkuchen-der-beste-Kaesekuchen-der-Welt.html') 
-update_json('https://www.chefkoch.de/rezepte/356221121015731/Putenschnitzel-mit-Brokkoli-vom-Blech.html')
-     
-
-
-
-
-        
+if __name__ == '__main__':
+    print("Programm started") 
+    formating_recipe('https://www.chefkoch.de/rezepte/1255131230975627/Traenenkuchen-der-beste-Kaesekuchen-der-Welt.html')
+    
+    # creating_json('https://www.chefkoch.de/rezepte/1255131230975627/Traenenkuchen-der-beste-Kaesekuchen-der-Welt.html') 
+    # update_json('https://www.chefkoch.de/rezepte/1137791220019690/Illes-leichter-und-leckerer-Thunfisch-Tomaten-Salat.html')
